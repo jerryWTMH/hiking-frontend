06a4d845478c55ef126c1b190353cdaf52681126
peter0512lee <peter0512lee@gmail.com>
2021-02-01 14:32:01 +0800

    call api not finish

--- src/pages/Register0-form.js
+++ src/pages/Register0-form.js
@@ -4,14 +4,39 @@ import BackButton from '../components/BackButton';
 import { useForm } from 'react-hook-form';
 import { useHistory } from "react-router-dom";
 
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+  })
+  .then(response => response.json()) // 輸出成 json
+}
 
 function Register0() {
   const { register, handleSubmit } = useForm();
 
   let history = useHistory();
   const handleClick = data => {
-    history.push("/register1");
+    history.push("/register1"); // turn page
     console.log(data);
+
+    postData('https://hiking-backend.herokuapp.com/api/register', data)
+    .then(data => console.log(data)) // JSON from `response.json()` call
+    .catch(error => console.error(error))
+
+    const token = data.token;
+
+    console.log('post finish');
   }
 
   return (
--- src/pages/Register1-form.js
+++ src/pages/Register1-form.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './Register1.css';
 import { useForm } from 'react-hook-form';
-import { useHistory } from "react-router-dom";
 
-function Register1() {
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+  })
+  .then(response => response.json()) // 輸出成 json
+}
+
+function Register1({ token }) {
   const { register, handleSubmit } = useForm();
 
-  let history = useHistory();
   const handleClick = data => {
-    // history.push("/register1");
     console.log(data);
+    postData('https://hiking-backend.herokuapp.com/api/profile', data)
+    .then(data => console.log(data)) // JSON from `response.json()` call
+    .catch(error => console.error(error))
   }
 
   return (
